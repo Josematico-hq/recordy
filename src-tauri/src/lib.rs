d4da569ac5d7887efc346ab4cdd656610cfdd2bf
Jose Carlos Fernandez <jcfernandez@theworkshop.com>
2025-12-11 12:14:48 +0100

    build: update dependencies and tauri configuration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
             use tauri::Manager;
